57bd23b30f7d358f16c6b8e380d5ef11c14dbd5e
Rodolfo Macbook <rodolfomacbook@rodo.local>
2020-05-20 13:15:25 -0500

    [ADD]module init

---
+++ imj/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'IMJ',
+    'version' : '1.1',
+    'summary': 'Init',
+    'sequence': 1,
+    'description': """
+            Init""",
+    'category': 'Base',
+    'website': 'https://www.odoo.com/',
+    'depends' : ['base'],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+}
