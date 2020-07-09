f7f52cac2c786677d4c45efc072efa703e374252
Rodolfo Macbook <rodolfomacbook@rodo.local>
2020-07-09 01:09:25 -0500

    [REF]add vat unique and required

---
+++ imj/models/partner.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    vat = fields.Char(string='Tax ID', required=True, help="The Tax Identification Number. Complete it if the contact is subjected to government taxes. Used in some legal statements.")
+
+	_sql_constraints = [
+        ('vat_uniq', 'unique (vat)', 'El RFC debe ser Unico!')
+    ]
