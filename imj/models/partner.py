e31614ab4c92570f6c01c40a69c2daccd93b8517
Rodolfo Macbook <rodolfomacbook@rodo.local>
2020-07-09 01:12:02 -0500

    [REF]add vat unique and required

--- imj/models/partner.py
+++ imj/models/partner.py
@@ -7,6 +7,6 @@ class ResPartner(models.Model):
 
     vat = fields.Char(string='Tax ID', required=True, help="The Tax Identification Number. Complete it if the contact is subjected to government taxes. Used in some legal statements.")
 
-	_sql_constraints = [
+    _sql_constraints = [
         ('vat_uniq', 'unique (vat)', 'El RFC debe ser Unico!')
     ]
