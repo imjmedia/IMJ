121f0b18f8c02ecbe0ff5e03c69e19f891360b5c
Rodolfo Macbook <rodolfomacbook@rodo.local>
2020-09-04 17:38:54 -0500

    [REF]add module budget purchase

---
+++ budget_purchase_imj/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Budget Purchase IMJ',
+    'version' : '1.1',
+    'summary': 'IMJ',
+    'sequence': 1,
+    'description': """
+            Module add validation of budget in purchase""",
+    'category': 'Account',
+    'website': 'https://www.odoo.com/',
+    'depends' : ['account','account_budget','purchase'],
+    'data': ['views/account_view.xml',
+            ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+}
