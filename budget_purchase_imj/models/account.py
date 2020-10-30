e41fa4de2a08a8301a06b2eae1a06a03b40ae96e
elrodo1 <rodolfo.lopez.t@gmail.com>
2020-10-30 14:24:13 -0600

    [REF]budget extras

--- budget_purchase_imj/models/account.py
+++ budget_purchase_imj/models/account.py
@@ -7,10 +7,37 @@ import odoo.addons.decimal_precision as dp
 
 
 
+class CrossoveredBudge(models.Model):
+    _inherit = "crossovered.budget"
+
+    start_date = fields.Date('Inicio')
+    end_date = fields.Date('Fin')
+
 class CrossoveredBudgetLines(models.Model):
     _inherit = "crossovered.budget.lines"
 
     amount_purchase = fields.Float(string='Importe Compras')
+    qty = fields.Float(string='Cantidad')
+    price = fields.Float(string='Precio')
+    duration = fields.Float(strin='Duración')
+    planned_amount = fields.Monetary(
+        'Planned Amount', required=True,
+        help="Amount you plan to earn/spend. Record a positive amount if it is a revenue and a negative amount if it is a cost.")
+    
+    def _compute_amount_imj(self):
+        for line in self:
+            if line.qty and line.price and line.duration:
+                line.planned_amount = line.qty * line.price * line.duration
+            else:
+                line.planned_amount = 0
+
+    @api.onchange('qty','price','duration')
+    def _onchange_planned(self):
+        for line in self:
+            if line.qty and line.price and line.duration:
+                line.planned_amount = line.qty * line.price * line.duration
+            else:
+                line.planned_amount = 0
 
     def _compute_purchase(self):
         for line in self:
