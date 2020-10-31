c47981b352bc36fed7bbd68f6ba698e6942d0047
elrodo1 <rodolfo.lopez.t@gmail.com>
2020-10-31 12:03:36 -0600

    [REF]add budget extras

--- budget_purchase_imj/models/account.py
+++ budget_purchase_imj/models/account.py
@@ -13,23 +13,36 @@ class CrossoveredBudge(models.Model):
     start_date = fields.Date('Inicio')
     end_date = fields.Date('Fin')
 
+    @api.onchange('start_date','end_date')
+    def _onchange_dates(self):
+        for budget in self:
+            if budget.start_date and budget.end_date:
+                days=fields.Date.from_string(budget.end_date) - fields.Date.from_string(budget.start_date)
+                for line in budget.crossovered_budget_line:
+                    line.duration=days.days
+
+
 class CrossoveredBudgetLines(models.Model):
     _inherit = "crossovered.budget.lines"
 
     amount_purchase = fields.Float(string='Importe Compras')
     qty = fields.Float(string='Cantidad')
     price = fields.Float(string='Precio')
-    duration = fields.Float(strin='Duración')
+    duration = fields.Integer(strin='Duración', compute='_compute_duration_imj')
     planned_amount = fields.Monetary(
         'Planned Amount', required=True,
         help="Amount you plan to earn/spend. Record a positive amount if it is a revenue and a negative amount if it is a cost.")
     
-    def _compute_amount_imj(self):
+
+
+    def _compute_duration_imj(self):
         for line in self:
-            if line.qty and line.price and line.duration:
-                line.planned_amount = line.qty * line.price * line.duration
+            if line.crossovered_budget_id.start_date and line.crossovered_budget_id.end_date:
+                days=fields.Date.from_string(line.crossovered_budget_id.end_date) - fields.Date.from_string(line.crossovered_budget_id.start_date)
+                line.duration=days.days
             else:
-                line.planned_amount = 0
+                line.duration=0
+
 
     @api.onchange('qty','price','duration')
     def _onchange_planned(self):
