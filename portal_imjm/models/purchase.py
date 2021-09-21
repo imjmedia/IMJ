4b4b9a16a471c5113e6484e86416847020b2f8c9
unknown <fmanime@gmail.com>
2021-09-21 17:14:02 -0500

    [FIX] agrega otra funcion

--- portal_imjm/models/purchase.py
+++ portal_imjm/models/purchase.py
@@ -28,9 +28,9 @@ class PurchaseOrder(models.Model):
                 continue
             # if not float_is_zero(line.qty_to_invoice, precision_digits=precision):
             if pending_section:
-                invoice_vals['invoice_line_ids'].append((0, 0, pending_section._prepare_account_move_line(line)))
+                invoice_vals['invoice_line_ids'].append((0, 0, pending_section._prepare_account_move_line_v14()))
                 pending_section = None
-            invoice_vals['invoice_line_ids'].append((0, 0, line._prepare_account_move_line(line)))
+            invoice_vals['invoice_line_ids'].append((0, 0, line._prepare_account_move_line_v14()))
         invoice_vals_list.append(invoice_vals)
 
         # if not invoice_vals_list:
@@ -100,3 +100,37 @@ class PurchaseOrder(models.Model):
             'company_id': self.company_id.id,
         }
         return invoice_vals
+
+
+    def _prepare_account_move_line_v14(self, move=False):
+        self.ensure_one()
+        aml_currency = move and move.currency_id or self.currency_id
+        date = move and move.date or fields.Date.today()
+        res = {
+            'display_type': self.display_type,
+            'sequence': self.sequence,
+            'name': '%s: %s' % (self.order_id.name, self.name),
+            'product_id': self.product_id.id,
+            'product_uom_id': self.product_uom.id,
+            'quantity': self.qty_to_invoice,
+            'price_unit': self.currency_id._convert(self.price_unit, aml_currency, self.company_id, date),
+            'tax_ids': [(6, 0, self.taxes_id.ids)],
+            'analytic_account_id': self.account_analytic_id.id,
+            'analytic_tag_ids': [(6, 0, self.analytic_tag_ids.ids)],
+            'purchase_line_id': self.id,
+        }
+        if not move:
+            return res
+
+        if self.currency_id == move.company_id.currency_id:
+            currency = False
+        else:
+            currency = move.currency_id
+
+        res.update({
+            'move_id': move.id,
+            'currency_id': currency and currency.id or False,
+            'date_maturity': move.invoice_date_due,
+            'partner_id': move.partner_id.id,
+        })
+        return res
